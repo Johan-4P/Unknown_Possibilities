db3f4aa9798c12f098594e15c7a8b6dcabfa6d10
Johan-4P <3db9470n@students.codeinstitute.net>
2025-04-07 12:34:37 +0200

    Refactor SECRET_KEY handling and remove static files directory definition

--- unknown_possibilities/settings.py
+++ unknown_possibilities/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d$pwa(bi2+x6k!7)+sf5#0psku8hel+h7h$%x1u=f$77!!l(zx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -167,7 +167,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory for collected static files
 
 # Default file storage for AWS S3
 if 'USE_AWS' in os.environ:
