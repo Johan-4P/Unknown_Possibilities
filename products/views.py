2908a6478a10663c0e8a0ad56d8516146007ab4a
Johan-4P <3db9470n@students.codeinstitute.net>
2025-04-08 18:34:11 +0200

    Implement product listing and detail views with templates; add navigation link for products

--- products/views.py
+++ products/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 # Create your views here.
 
-def product(request):
-    """ A view to return the products page """
-    return render(request, 'product.html')
+def all_product(request):
+    """ A view to return all products """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+def products_detail(request, product_id):
+    """ A view to return a single product detail page """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
