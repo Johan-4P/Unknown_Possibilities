2fab8039fb78e6657dfae4f91e7c89d02178dc63
Johan-4P <3db9470n@students.codeinstitute.net>
2025-04-10 14:42:20 +0200

    Implement search functionality with error handling and update form actions

--- products/views.py
+++ products/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Product, TarotCard, Category
+from django.contrib import messages
 from django.db.models import Q
 import random
 
 def all_product(request):
     """ A view to return all products """
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries).distinct()
 
     context = {
         'products': products,
+        'search_term': query,
     }
     return render(request, 'products/products.html', context)
 
