052b5ecf15124d8f2388f7555fe40b5ed2c10909
Johan-4P <3db9470n@students.codeinstitute.net>
2025-04-21 11:40:35 +0200

    Add UserProfileAdmin and OrderInline to manage user profiles and associated orders in the admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+from .models import UserProfile
+from checkout.models import Order
+
+class OrderInline(admin.TabularInline):
+    model = Order
+    fields = ('order_number', 'full_name', 'email', 'grand_total', 'date')
+    readonly_fields = fields
+    extra = 0
+    can_delete = False
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'default_phone_number', 'default_country', 'default_town_or_city')
+    inlines = (OrderInline,)
